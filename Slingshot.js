79c1b8fd072c853c9734d1216ec668ed762c0f0c
aashu3012 <73949515+aashu3012@users.noreply.github.com>
2021-07-28 20:08:03 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -1,29 +1,31 @@
-class Slingshot{
-    constructor(bodyA, pointB){
+class Slingshot {
+    constructor(bodyA, pointB) {
         var options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
-        this.pointB=pointB
         this.sling = Constraint.create(options);
+        this.pointB = pointB;
         World.add(world, this.sling);
+
     }
-
-    fly(){
-        this.sling.bodyA=null
+    attach(body){
+        this.sling.bodyA = body;
     }
-
-    display(){
-        if(this.sling.bodyA){
-
+    fly() {
+        this.sling.bodyA = null;
+    }
+    display() {
+        if (this.sling.bodyA) {
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
 
+            strokeWeight(4);
+            stroke("turquoise");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
-        
     }
+
 }
--- sketch.js
+++ sketch.js
@@ -4,74 +4,83 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var holder,ball,ground;
-var stand1,stand2;
+var holder, ball, ground;
+var stand1, stand2;
 var ball;
-var slingShot;
+var slingshot;
 var polygon;
 
+function preload(){
+  polygonImg = loadImage("polygon.png");
+}
 function setup() {
-  createCanvas(900,400);
+  createCanvas(900, 400);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
   ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
- 
- 
+  stand1 = new Stand(390, 300, 250, 10);
+  stand2 = new Stand(700, 200, 200, 10);
+
+
   //level one
-  block1 = new Block(300,275,30,40);
+  block1 = new Block(300, 275, 30, 40);
   console.log(block1);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
+  block2 = new Block(330, 275, 30, 40);
+  block3 = new Block(360, 275, 30, 40);
+  block4 = new Block(390, 275, 30, 40);
+  block5 = new Block(420, 275, 30, 40);
+  block6 = new Block(450, 275, 30, 40);
+  block7 = new Block(480, 275, 30, 40);
   //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
+  block8 = new Block(330, 235, 30, 40);
+  block9 = new Block(360, 235, 30, 40);
+  block10 = new Block(390, 235, 30, 40);
+  block11 = new Block(420, 235, 30, 40);
+  block12 = new Block(450, 235, 30, 40);
   //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
+  block13 = new Block(360, 195, 30, 40);
+  block14 = new Block(390, 195, 30, 40);
+  block15 = new Block(420, 195, 30, 40);
   //top
-  block16 = new Block(390,155,30,40);
+  block16 = new Block(390, 155, 30, 40);
   //bottom
-  block17= new Block(630,115,30,40);
-  block18= new Block(660,115,30,40);
-  block19= new Block(690,115,30,40);
-  block20= new Block(720,115,30,40);
-  block21= new Block(750,115,30,40);
+  block17 = new Block(630, 115, 30, 40);
+  block18 = new Block(660, 115, 30, 40);
+  block19 = new Block(690, 115, 30, 40);
+  block20 = new Block(720, 115, 30, 40);
+  block21 = new Block(750, 115, 30, 40);
   //middle
-  block22= new Block(660,75,30,40);
-  block23= new Block(690,75,30,40);
-  block24= new Block(720,75,30,40);
-//top
-block25= new Block(690,35,30,40);
+  block22 = new Block(660, 75, 30, 40);
+  block23 = new Block(690, 75, 30, 40);
+  block24 = new Block(720, 75, 30, 40);
+  //top
+  block25 = new Block(690, 35, 30, 40);
 
-polygon = new Polygon(50,200)
-slingshot= new Slingshot(polygon.body,{x:50,y:180})
+  polygon = Bodies.circle(100,250,25);
+  World.add(world,polygon);
+
+  slingshot = new Slingshot(polygon, {
+    x: 100,
+    y: 250
+  });
 
 
-  
+
 
 }
+
 function draw() {
-  background(56,44,44); 
- 
+  background(56, 44, 44);
+
   textSize(20);
   fill("lightyellow");
-  
+
 
   ground.display();
   stand1.display();
   stand2.display();
-  
+
   strokeWeight(2);
   stroke(15);
   fill("skyblue");
@@ -100,16 +109,23 @@ function draw() {
   block19.display();
   block20.display();
   block21.display();
-fill("turquoise")
-block22.display();
-block23.display();
-block24.display();
-fill("pink")
-block25.display();
+  fill("turquoise")
+  block22.display();
+  block23.display();
+  block24.display();
+  fill("pink")
+  block25.display();
 
-polygon.display();
-slingshot.display();
+  
+  image(polygonImg, polygon.position.x, polygon.position.y, 40,40);
+  slingshot.display();
 
- 
+}
 
+function mouseDragged(){
+  Matter.Body.setPosition(polygon, {x: mouseX, y: mouseY});
+}
+
+function mouseReleased(){
+  slingshot.fly();
 }
